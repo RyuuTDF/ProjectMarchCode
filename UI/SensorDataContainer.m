50fa24195bce47caa4b73fda57af605ceded5227
Vasco de Bruijn <vascodebruijn@gmail.com>
2016-05-02 17:33:39 +0200

    Added Graphs and Updates

--- UI/SensorDataContainer.m
+++ UI/SensorDataContainer.m
@@ -1,4 +1,4 @@
-classdef SensorDataContainer
+classdef SensorDataContainer <  handle
     %SENSORDATACONTAINER Contains the sensor data and methods to convert
     %the data to certain formats
     
@@ -13,6 +13,16 @@ classdef SensorDataContainer
     methods
         function matrix = SensorDataContainer(input)
             matrix.datamatrix = input;          
+        end
+        
+        function out = returnColumn(obj,idx)
+            out = obj.datamatrix(:,idx);
+        end
+       function setColumn(obj,idx,data)
+            obj.datamatrix(:,idx) = data;
+        end
+        function out = returnRow(obj,idx)
+            out = obj.datamatrix(idx,:);
         end
     end
     
--- UI/SimpleGui.m
+++ UI/SimpleGui.m
@@ -1,24 +1,56 @@
-classdef SimpleGui
+classdef SimpleGui <handle 
     %SIMPLEGUI Shows the sensor data and labels in a table
     
     properties
-        sensorlabel = ['Sensor1';'Sensor2'];
-        sensortype = ['Encoder'; '   Goat'];
-        sensordata = [0,0];
+        sensorlabel = {};
+        sensortype = {};
+        importantSensors = [];
+        impSensors;
+        allSensors;
+        graph;
+        graph2;
     end
     
     methods(Static)
+        function streamTest
+            data = SensorDataContainer(importdata('TestData.mat'));
+            gui= SimpleGui(data,[1,3]);
+            databacklog = transpose(data.returnColumn([1,3]))
+            while true
+                rndVals = cellfun(@(x) x*(rand(1)+0.5),data.returnColumn(3),'un',0);
+                data.setColumn(3,transpose(rndVals));
+                gui.update(data);
+                databacklog = vertcat(databacklog,transpose(data.returnColumn(3)));
+                axes(gui.graph2);
+                plot(cell2mat(transpose(databacklog(2:end,1))));
+                axes(gui.graph);
+                plot(cell2mat(transpose(databacklog(2:end,2))));
+                pause(0.02);
+            end
+        end
     end
     
     methods 
-        function gui = SimpleGui(input)
+        function gui = SimpleGui(sensorData, importantSensors)
+            close all;
             if(nargin >0)
-                gui.sensorlabel = input{1,1};
-                gui.sensortype = input{1,2};                
-                gui.sensordata = input{1,3};
-                f = figure('Position', [100 100 752 250]);
-                t = uitable('Parent', f, 'Position', [25 50 700 200], 'Data',input)
+                 gui.sensorlabel = sensorData.returnColumn(1);
+                 gui.sensortype = sensorData.returnColumn(2);
+                 gui.importantSensors = importantSensors;
+                 
+                rootFig = figure('Position', [100 100 800 800]);
+                gui.graph = axes('Units','pixels','Position',[25,25,300,300]);
+                gui.graph.Units = 'normalized';
+                gui.graph2 = axes('Units','pixels','Position',[375,25,300,300]);
+                gui.graph2.Units = 'normalized';
+                
+                gui.impSensors = uitable('Parent', rootFig, 'Position', [0 350 300 250], 'Data',sensorData.datamatrix(importantSensors,:),'RowName',[],'ColumnName',[]);                
+                gui.allSensors = uitable('Parent', rootFig, 'Position', [250 350 300 250], 'Data',sensorData.datamatrix,'RowName',[],'ColumnName',[]);            
             end
+        end
+        function update(gui,data)
+            gui.impSensors.Data = data.datamatrix(gui.importantSensors,:);
+            gui.allSensors.Data = data.datamatrix;
         end
         
     end
