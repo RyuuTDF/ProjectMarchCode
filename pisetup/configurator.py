5d65fab1fcf13be37edad8aff1aed7fe5ad82c4d
Ruben <r.visser@lunoct.nl>
2016-05-04 14:42:21 +0200

    Configurator: first version that should actually work

--- pisetup/configurator.py
+++ pisetup/configurator.py
@@ -10,36 +10,76 @@ import shutil
 
 
 def run():
-    config = open('filelist', 'r')
-    for line in config:
-        if line[0] == '#':
-            continue
-        (command, source, destination) =  tuple(line[:-1].split("\t", 3))
-        print "Command " + command + " for file " + source + " found: destination " + destination
-	if command == 'modify':
-		(something)
-		if path.exists(destination):
-			shutil.copy(destination, destination + ".bak")
-			
-	elif command == 'append':
-		(something)
-		if path.exists(destination):
-			shutil.copy(destination, destination + ".bak")
+	config = open('filelist', 'r')
+	for line in config:
+		if line[0] == '#':
+			continue
+		(command, source, destination) =  tuple(line[:-1].split("\t", 3))
+		if command == 'modify':
+			if os.path.exists(destination):
+				shutil.move(destination, destination + ".bak")
+				print "Applying modifications to " + source
+				replaceList = readReplacements(source)
+				with open(destination, 'w') as output:
+					with open(destination + ".bak") as input:
+						for inputLine in input:
+							output.write(replaceWithList(inputLine, replaceList))
+			else:
+				print "ERROR: can't modify non-existing file " + source + "!"
+				
+		elif command == 'append':
+			if not checkPathFor(destination):
+				continue
+			if os.path.exists(destination):
+				shutil.copy(destination, destination + ".bak")
+			else:
+				print "Warning: appending to non-existing file " + source + " (new file will be created)"
+			print "Adding configuration options to " + source
+			with open(destination, 'a') as output:
+				with open(source) as input:
+					for inputLine in input:
+						output.write(inputLine)
+		else:
+			if not checkPathFor(destination):
+				continue
+			if os.path.exists(destination):
+				shutil.move(destination, destination + ".bak")
+			print "Replacing file " + source
+			shutil.copy(source, destination)
+	config.close()
+
+
+def replaceWithList(target, replaceList):
+	if len(replaceList) == 0:
+		return target
 	else:
-		(replace)
-		checkPathFor(destination)
-		if path.exists(destination):
-			shutil.move(destination, destination + ".bak")
-		shutil.copy(source, destination)
-		
+		last = replaceList.pop()
+		return replaceWithList(target.replace(last[0], last[1]), replaceList)
+
+
+def readReplacements(input):
+	resultList = []
+	with open(input) as file:
+		skip = 2
+		line = file.readline()
+		while line != '':
+			if skip > 0:
+				skip = skip - 1
+			else:
+				resultList.append((line[:-1], file.readline()[:-1]))
+			line = file.readline()
+	return resultList
+
 
 ## def checkPathFor
 # Will check if path exists for filename, and if not try to create required directories
 # @param filename
 def checkPathFor(filename):
-	directory = realpath(filename)
-	if ! isdir(directory):
-		
+	directory = os.path.dirname(filename)
+	if not os.path.exists(directory):
+		os.makedirs(directory)
+	return True;
+
 
 if __name__ == "__main__":
     run()
