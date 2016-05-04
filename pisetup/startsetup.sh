31c65d6353b541f609ac82cfeb22920bfcce9133
Ruben <r.visser@lunoct.nl>
2016-05-04 17:13:52 +0200

    Line endings arent supported in sh

--- pisetup/startsetup.sh
+++ pisetup/startsetup.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 # -------------------------------------------------------------
 # Auto installation tool for Raspberry Pi to be used on the
 # Project MARCH exoskeleton as connection bridge to an external
@@ -14,7 +14,7 @@ fi
 read -p $'Auto installation tool for Raspberry Pi to be used on the Project MARCH exoskeleton as connection bridge to an external monitoring client.\nDo only run this tool on a designated Raspberry Pi! It might break any other system.\nPress CTRL+C to abort, or ENTER to continue.'
 
 # Warning: internet connection required
-read -rp $'\n Please make sure the Raspberry Pi is connected to the internet. This tool will automatically download and install required software. In addition, make sure to run this tool on a local terminal or in a screen session. A remote session will be interrupted during network reconfiguration and might break the installation. \nPress ENTER to continue'
+read -p $'\n Please make sure the Raspberry Pi is connected to the internet. This tool will automatically download and install required software. In addition, make sure to run this tool on a local terminal or in a screen session. A remote session will be interrupted during network reconfiguration and might break the installation. \nPress ENTER to continue'
 
 printf "Updating software lists...\n"
 apt-get -qq update
@@ -41,4 +41,4 @@ systemctl enable hostapd
 systemctl enable dnsmasq
 
 
-read -rp $'Installation complete.\nPlease disconnect the Raspberry Pi from the current network. Leaving the Raspberry Pi connected will cause malfunctions on the network.\nAfter disconnecting, press ENTER to reboot and activate the configuration.'
+read -p $'Installation complete.\nPlease disconnect the Raspberry Pi from the current network. Leaving the Raspberry Pi connected will cause malfunctions on the network.\nAfter disconnecting, press ENTER to reboot and activate the configuration.'
