021a269686725e17690471457290d5eb13bb95a4
Jens <racingdutch@hotmail.com>
2016-05-10 10:48:04 +0200

    Split up tests

--- UDP Test/NetworkTest.m
+++ UDP Test/NetworkTest.m
@@ -1,4 +1,4 @@
-%Initialize Receiveroutput
+%Initialize the receiver
 IPReceive = '0.0.0.0';
 receiver = dsp.UDPReceiver('RemoteIPAddress', IPReceive,'MaximumMessageLength',65507);
 bytesReceived = 0;
--- UDP Test/UDPTestReceive.m
+++ UDP Test/UDPTestReceive.m
@@ -1,20 +1,21 @@
 %Uncomment below line to test using local data.
-%Localtest.m
+%Localtest
 %Uncomment below line to test using the network.
 NetworkTest
 
+%Testing Purposes
 i = 1;
 
 while true
     %Receive the packet
     received_data = step(receiver);
-    %If there is a packet, print it.
+    %If there is a packet, deserialize it.
     if ~isempty(received_data)
       bytesReceived = length(received_data);
       deserialized_data = data_deserialize(received_data);
+      
+      %Testing Purposes
       fprintf('Packet: %d\n', i);
-      %fprintf('Bytes received: %d\n', bytesReceived);
-      %'I have received stuff'
       i = i+1;
     end
     pause(0.001)
