2641b6106d518f17f73520fbb1e370e9be3c9778
Ruben <r.visser@lunoct.nl>
2016-05-13 12:15:17 +0200

    Restore the terminal properly on exit

--- pisetup/statuscli.py
+++ pisetup/statuscli.py
@@ -9,7 +9,7 @@ from tabulate import tabulate
 import curses
 import subprocess
 
-def main():
+def main(stdscr):
 	# Open the file for reading
 	fd = os.open('/run/exoshm', os.O_RDONLY)
 	
@@ -18,17 +18,15 @@ def main():
 	
 	connected = None
 	packetRate = None
-	stdscr = curses.initscr()
-	
 	while 1:
 		new_c, = struct.unpack('i', buf[:4])
-		new_p, = struct.unpack('f', buf[4:8])
+		new_p, = struct.unpack('d', buf[4:12])
 		
 		if connected != new_c or packetRate != new_p:
 			p = subprocess.Popen(['/opt/vc/bin/vcgencmd', 'measure_temp'], stdout=subprocess.PIPE, 
 			stderr=subprocess.PIPE)
 			out, err = p.communicate()
-			text = tabulate([[new_c, new_p, out[5:]], headers=['Connected','Packets/s', 'Temperature'])
+			text = tabulate([[new_c, new_p, out[5:]]], headers=['Connected','Packets/s', 'Temperature'])
 			stdscr.addstr(0, 0, text)
 			stdscr.refresh()
 			connected = new_c
@@ -38,4 +36,4 @@ def main():
 
 
 if __name__ == '__main__':
-	main()
+	curses.wrapper(main)
