c4b5002b5332cad44fb58fc8dd18637fe0cf1cfc
Jens <racingdutch@hotmail.com>
2016-05-23 14:55:53 +0200

    Added pointers

--- UI/NetworkEnv.m
+++ UI/NetworkEnv.m
@@ -3,6 +3,7 @@ classdef NetworkEnv < Env
 	%   Setup the environment for network usage of data
 
 	properties
+        sender
         receiver
     end
 
@@ -10,8 +11,16 @@ classdef NetworkEnv < Env
         % Function: NetworkEnv
         % Functionality: Constructs the Network Environment
         function obj = NetworkEnv()
+            %Send for reference packet.
+            obj.sender = dsp.UDPSender('192.168.21.1',25001);
+            % dataSent = 'Maak de data hier aan'
+            % step(obj.sender, dataSent); <- Verstuurd de data
+            
+            %Receive the first packet
             obj.receiver = dsp.UDPReceiver('RemoteIPAddress', '0.0.0.0','MaximumMessageLength',65507);
-             while isempty(obj.currentData)
+            
+            %Bouw onderstaande om naar iets dat alles behalve reference paccket negeert.           
+            while isempty(obj.currentData)
                 obj = updateData(obj);
             end
         end
@@ -19,6 +28,7 @@ classdef NetworkEnv < Env
         % Function: updateData
         % Functionality: Update the current data set if a new packet has arrived.
         function obj = updateData(obj)
+            %TO DO: Ombouwen dat deze de delta ondersteund.
             packet = step(obj.receiver);
             if ~isempty(packet)
                 packetdata = deserialize(packet);
