503f73a25b04b2ec067765f171fb7e2e0afa4bc2
Vasco de Bruijn <vascodebruijn@gmail.com>
2016-06-17 09:59:34 +0200

    removed testmaps

--- Sprint 2 Demo/timedOut.m
+++
@@ -1,9 +0,0 @@
-% Function: timedOut
-% Functionality: Resend request for new reference packet.
-function timedOut(~, ~, obj)
-	step(obj.sender, obj.lastDeltaChecksum);
-	
-    %Testing Purposes
-    fprintf('TIMEOUT!\n');
-end
-
